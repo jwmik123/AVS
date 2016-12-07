7f4dc14f203e751a9e412d8d780f36cf09ced733
jwmik123 <joelmik123@gmail.com>
2016-12-07 13:07:45 +0100

    posts

--- header.php
+++ header.php
@@ -8,3 +8,17 @@
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/font-awesome/css/font-awesome.css">
   </head>
 <?php include_once('functions.php'); ?>
+  <div class="socials">
+      <span id="fb">FB</span>
+      <span id="phone">0623521980</span>
+      <span id="language">Language</span>
+  </div>
+  <div class="navigation">
+      <ul>
+          <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Home' ) ); ?></li>
+          <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Diensten' ) ); ?></li>
+          <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Over' ) ); ?></li>
+          <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Contact' ) ); ?></li>
+
+      </ul>
+  </div>
--- index.php
+++ index.php
@@ -2,21 +2,6 @@
 
 <body>
 
-<div class="socials">
-    <span id="fb">FB</span>
-    <span id="phone">0623521980</span>
-    <span id="language">Language</span>
-</div>
-<div class="navigation">
-    <ul>
-        <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Home' ) ); ?></li>
-        <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Diensten' ) ); ?></li>
-        <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Over' ) ); ?></li>
-        <li id="li-link"><?php wp_nav_menu( array( 'theme_location' => 'Contact' ) ); ?></li>
-
-    </ul>
-</div>
-
 <div class="slider">
     <?php
     echo do_shortcode("[metaslider id=4]");
@@ -25,19 +10,41 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
-<div class="over-avs text-center">
-    <?php
-    $post_id = 1;
-    $queried_post = get_post($post_id);
-    $title = $queried_post->post_title;
-    echo "<div class='row'>";
-    echo "<div class='col'"
-    echo "<h3 id='post-title'>".$title."</h3>";
-    echo "<p>".$queried_post->post_content."</p>";
+            <div class="over-avs text-center">
 
-    ?>
-</div>
-
+                <?php
+                    $post_id = 22;
+                    $queried_post = get_post($post_id);
+                    $title = $queried_post->post_title;
+                    echo "<div class='row'>";
+                    echo "<div class='col-md-3'>";
+                    echo "<h3 id='post-title'>".$title."</h3>";
+                    echo "<p>".$queried_post->post_content."</p>";
+                    echo "</div>";
+                    $post_id = 23;
+                    $queried_post = get_post($post_id);
+                    $title = $queried_post->post_title;
+                    echo "<div class='col-md-3'>";
+                    echo "<h3 id='post-title'>".$title."</h3>";
+                    echo "<p>".$queried_post->post_content."</p>";
+                    echo "</div>";
+                    $post_id = 25;
+                    $queried_post = get_post($post_id);
+                    $title = $queried_post->post_title;
+                    echo "<div class='col-md-3'>";
+                    echo "<h3 id='post-title'>".$title."</h3>";
+                    echo "<p>".$queried_post->post_content."</p>";
+                    echo "</div>";
+                    $post_id = 27;
+                    $queried_post = get_post($post_id);
+                    $title = $queried_post->post_title;
+                    echo "<div class='col-md-3'>";
+                    echo "<h3 id='post-title'>".$title."</h3>";
+                    echo "<p>".$queried_post->post_content."</p>";
+                    echo "</div>";
+                    echo "</div>";
+                ?>
+            </div>
     </div>
 </div>
 </div>
