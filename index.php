4fda24e5d0ce46d224affa700732be6ddfba607e
jwmik123 <joelmik123@gmail.com>
2016-12-07 13:22:00 +0100

    posts

--- index.php
+++ index.php
@@ -13,17 +13,10 @@
             <div class="over-avs text-center">
 
                 <?php
-                    $post_id = 22;
+                    $post_id = 21;
                     $queried_post = get_post($post_id);
                     $title = $queried_post->post_title;
                     echo "<div class='row'>";
-                    echo "<div class='col-md-3'>";
-                    echo "<h3 id='post-title'>".$title."</h3>";
-                    echo "<p>".$queried_post->post_content."</p>";
-                    echo "</div>";
-                    $post_id = 23;
-                    $queried_post = get_post($post_id);
-                    $title = $queried_post->post_title;
                     echo "<div class='col-md-3'>";
                     echo "<h3 id='post-title'>".$title."</h3>";
                     echo "<p>".$queried_post->post_content."</p>";
@@ -36,6 +29,13 @@
                     echo "<p>".$queried_post->post_content."</p>";
                     echo "</div>";
                     $post_id = 27;
+                    $queried_post = get_post($post_id);
+                    $title = $queried_post->post_title;
+                    echo "<div class='col-md-3'>";
+                    echo "<h3 id='post-title'>".$title."</h3>";
+                    echo "<p>".$queried_post->post_content."</p>";
+                    echo "</div>";
+                    $post_id = 29;
                     $queried_post = get_post($post_id);
                     $title = $queried_post->post_title;
                     echo "<div class='col-md-3'>";
